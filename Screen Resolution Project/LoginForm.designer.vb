de04ff77f4b19bd66fdafaf4a5f5e12186fa3175
smartgaastech <smartgaastech@gmail.com>
2018-01-02 21:53:25 +0400

    Fixed Tax using analysis code
    Fixed Tax using analysis code

--- Screen Resolution Project/LoginForm.designer.vb
+++ Screen Resolution Project/LoginForm.designer.vb
@@ -114,9 +114,9 @@ Partial Class LoginForm
         Me.Label6.ForeColor = System.Drawing.Color.Maroon
         Me.Label6.Location = New System.Drawing.Point(9, 186)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(92, 14)
+        Me.Label6.Size = New System.Drawing.Size(80, 14)
         Me.Label6.TabIndex = 12
-        Me.Label6.Text = "Version: 5.12.31"
+        Me.Label6.Text = "Version: 6.1.2"
         '
         'butLoginCancel
         '
